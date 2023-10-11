a9ff24dcfd0764d5d9ad4df58ec197e674a9453a
student <student@intranet.proweb.uz>
2023-10-11 18:27:56 +0500

    added article model displayed articles on home page customized admin for article

--- blog/web_site/models.py
+++ blog/web_site/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
 
@@ -8,3 +11,29 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name= "Категория"
+        verbose_name_plural = "Категории"
+
+class Article(models.Model):
+    title  = models.CharField(verbose_name="Заголовок статьи",  max_length= 155, unique = True)
+    short_description = models.TextField(verbose_name="Краткое содержание", max_length=225)
+    full_description= models.TextField(verbose_name="Полное описание")
+    photo = models.ImageField(verbose_name="Фото", upload_to="photos/articles/")
+    views = models.IntegerField(verbose_name="оличество просмотров", default=9)
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at= models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор", related_name="articles")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категории", related_name="articles")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural="Статья"
+
+
+
+
